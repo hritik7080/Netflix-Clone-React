5a16500e9ae5a5e264fd1d50b93d33737abd5e43
Hritik Gupta <hritikgupta7080@gmail.com>
2020-11-21 20:31:24 +0530

    Created Banner

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Nav.css"
+
+function Nav(){
+    return (
+        <div className="nav">
+            <img className="nav__logo" src="https://www.brandvia.com/wp-content/uploads/2019/04/Netflix-Logo.png" alt="Netflix Logo"/>
+
+            <img className="nav__avatar" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png" alt="Avatar"/>
+        </div>
+    )
+}
+
+export default Nav;
