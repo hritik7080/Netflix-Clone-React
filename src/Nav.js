60734d04f32f14536516bf15618073316646617c
Hritik Gupta <hritikgupta7080@gmail.com>
2020-11-21 21:05:59 +0530

    Added Navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Nav.css"
 
 function Nav(){
+
+    const [show, handleShow] = useState(false);
+
+    useEffect(()=>{
+        window.addEventListener("scroll", ()=>{
+            if(window.scrollY>100){
+                handleShow(true);
+            }
+            else{
+                handleShow(false);
+            }
+        });
+        return ()=>{
+            window.removeEventListener("scroll");
+        };
+
+    }, []);
+
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav__black"}`}>
             <img className="nav__logo" src="https://www.brandvia.com/wp-content/uploads/2019/04/Netflix-Logo.png" alt="Netflix Logo"/>
 
             <img className="nav__avatar" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png" alt="Avatar"/>
